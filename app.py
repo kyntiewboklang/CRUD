3742eaf18de6f9f9c27047e3f7af1b029764e881
kyntiewboklang <riselucky30@gmail.com>
2026-06-23 15:20:57 +0530

    update

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+import psycopg2
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="library_db",
+        username="postgres",
+        password="1234"
+    )
+
+    return conn
 
 @app.route("/")
 def home():
@@ -10,6 +20,10 @@ def home():
 @app.route("/add-book")
 def add_book():
     return render_template("add_book.html")
+
+@app.route("/view_book")
+def view_book():
+    return render_template("view_book.html")
 
 if __name__ == "__main__":  #important code, it is use for running the app
     app.run(debug=True)
