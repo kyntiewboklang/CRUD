1cd49691e11fafe5ab7eb5b587e661a6dbbb5d8a
kyntiewboklang <riselucky30@gmail.com>
2026-06-25 14:48:10 +0530

    updates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 app = Flask(__name__)
@@ -36,13 +36,43 @@ def create_table():
 def home():
     return render_template("index.html")
 
-@app.route("/add-book")
+@app.route("/add-book", methods=["GET","POST"])
 def add_book():
+    if request.method=="POST":
+        title = request.form["title"]
+        author = request.form["author"]
+        category = request.form["category"]
+        status = request.form["status"]
+
+        print(title, author, category, status)
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+                       INSERT INTO books (title, author, category, status)
+                       VALUES (%s, %s, %s, %s)
+                       """ , (title, author, category, status))
+        
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        return redirect("/view_book")
     return render_template("add_book.html")
 
 @app.route("/view_book")
 def view_book():
-    return render_template("view_book.html")
+    conn = get_db_connection()
+    cursor = conn.cursor() 
+
+    cursor.execute("SELECT * FROM books ORDER BY id ASC")
+    books = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return render_template("view_book.html", books=books)
 
 if __name__ == "__main__":  #important code, it is use for running the app
     create_table() 
